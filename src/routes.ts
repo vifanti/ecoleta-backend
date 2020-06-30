52d3cf72a963e74e1df827aae2738bed83842d5c
Vinicius Fantinatto <vifanti@gmail.com>
2020-06-30 20:14:29 -0300

    configuraçao do banco de dados, migrations e seeds

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,12 @@
 import express from 'express';
-// import knex from './database/connection';
+import knex from './database/connection';
 
 const routes = express.Router();
 
 routes.get('/items', async (request, response) => {
-  // const items = await knex('items').select('*');
+  const items = await knex('items').select('*');
 
-  return response.json({ message: 'hello' });
+  return response.json(items);
 });
 
 export default routes;
