eeb83b83b4c3e7556b345606bc76c92c391a33fc
Vinicius Fantinatto <vifanti@gmail.com>
2020-07-01 00:45:30 -0300

    controllers e rotas

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,16 @@
 import express from 'express';
-import knex from './database/connection';
+
+import PointsController from './controllers/PointsController';
+import ItemsController from './controllers/ItemsController';
 
 const routes = express.Router();
+const pointsController = new PointsController();
+const itemsController = new ItemsController();
 
-routes.get('/items', async (request, response) => {
-  const items = await knex('items').select('*');
+routes.get('/items', itemsController.index);
 
-  return response.json(items);
-});
+routes.post('/points', pointsController.create);
+routes.get('/points', pointsController.index);
+routes.get('/points/:id', pointsController.show);
 
 export default routes;
