2a72cc18afaa2457e253e1abfd04874f3b7a98f7
Vinicius Fantinatto <vifanti@gmail.com>
2020-07-02 03:33:46 -0300

    deploy

--- src/controllers/ItemsController.ts
+++ src/controllers/ItemsController.ts
@@ -9,7 +9,7 @@ class ItemsController {
       return {
         id: item.id,
         title: item.title,
-        image_url: `${process.env.APP_API_URL}:${process.env.PORT}/uploads/${item.image}`,
+        image_url: `${process.env.APP_API_URL}/uploads/${item.image}`,
       };
     });
 
